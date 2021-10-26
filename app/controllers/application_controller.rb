355475328a479c203275281e46b6c5de1babc343
EC2 Default User <ec2-user@ip-172-31-37-51.ec2.internal>
2021-10-26 08:58:20 +0000

    save

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  before_action :confiure_permitted_parameters, if: :devise_controller?
+
+
+  private
+
+  def confiure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+  end
+
 end
